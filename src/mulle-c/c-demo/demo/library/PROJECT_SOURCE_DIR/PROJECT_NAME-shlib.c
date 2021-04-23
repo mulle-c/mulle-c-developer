81a40bf0b75abc816b1680e4c4502c2049503f7a
Nat! <nat@codeon.de>
2021-04-23 15:34:20 +0200

    improved support for Frameworks

--- src/mulle-c/c-demo/demo/library/PROJECT_SOURCE_DIR/PROJECT_NAME-shlib.c
+++ src/mulle-c/c-demo/demo/library/PROJECT_SOURCE_DIR/PROJECT_NAME-shlib.c
@@ -4,5 +4,8 @@
  *
  *    rm <|PROJECT_NAME|>_shlib.c
  *    mulle-sde reflect
+ *
+ * template: demo/library/PROJECT_SOURCE_DIR/PROJECT_NAME-shlib.h
+ * vendor/extension: mulle-c-developer/c-demo
  */
 int   __<|PROJECT_DOWNCASE_IDENTIFIER|>_unused__ = 1848;
