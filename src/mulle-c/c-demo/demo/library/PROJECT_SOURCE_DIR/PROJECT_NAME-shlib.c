d15740923a5c6393eb386ea4e47d4ecc19fe094f
Nat! <nat@mulle-kybernetik.com>
2021-11-19 23:45:41 +0100

    remove obsolete template info

--- src/mulle-c/c-demo/demo/library/PROJECT_SOURCE_DIR/PROJECT_NAME-shlib.c
+++ src/mulle-c/c-demo/demo/library/PROJECT_SOURCE_DIR/PROJECT_NAME-shlib.c
@@ -4,8 +4,5 @@
  *
  *    rm <|PROJECT_NAME|>_shlib.c
  *    mulle-sde reflect
- *
- * template: demo/library/PROJECT_SOURCE_DIR/PROJECT_NAME-shlib.h
- * vendor/extension: mulle-c-developer/c-demo
  */
 int   __<|PROJECT_DOWNCASE_IDENTIFIER|>_unused__ = 1848;
